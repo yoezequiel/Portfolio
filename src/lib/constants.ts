888358dba000a668f5ae14d8696a3116d5d724bb
yoezequiel <ezequielcasiano15@gmail.com>
2024-02-14 15:58:00 -0300

    first commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const LINKS = {
+  github: "https://github.com/yoezequiel",
+  linkedin: "https://www.linkedin.com/in/ezequiel-casiano/",
+  twitter: "https://twitter.com/yoezequiel_",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
