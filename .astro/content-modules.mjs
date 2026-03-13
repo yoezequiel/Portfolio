f063a4a1c827b12a7737b0a671cf6e47fa95ea09
yoezequiel <ezequielcasiano15@gmail.com>
2026-03-13 14:01:33 -0300

    Refactor code structure for improved readability and maintainability

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,5 @@
 
 export default new Map([
-["src/content/projects/corebiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fcorebiz.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/crobf-infrastructure.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fcrobf-infrastructure.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/ecommerce-android-app.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fecommerce-android-app.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/github-metrics-engine.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fgithub-metrics-engine.mdx&astroContentModuleFlag=true")],
