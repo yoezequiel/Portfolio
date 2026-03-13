f5d1f58503b4ea69db99814e2c1beac0d5883850
yoezequiel <ezequielcasiano15@gmail.com>
2026-03-13 14:14:13 -0300

    chore: downgrade astro version from 6.0.4 to 5.0.0 in package.json

--- .astro/content-modules.mjs
+++
@@ -1,8 +0,0 @@
-
-export default new Map([
-["src/content/projects/crobf-infrastructure.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fcrobf-infrastructure.mdx&astroContentModuleFlag=true")],
-["src/content/projects/ecommerce-android-app.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fecommerce-android-app.mdx&astroContentModuleFlag=true")],
-["src/content/projects/github-metrics-engine.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fgithub-metrics-engine.mdx&astroContentModuleFlag=true")],
-["src/content/projects/pos-web.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpos-web.mdx&astroContentModuleFlag=true")],
-["src/content/projects/unica-sistema-de-gestion-comercial.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Funica-sistema-de-gestion-comercial.mdx&astroContentModuleFlag=true")]]);
-		
